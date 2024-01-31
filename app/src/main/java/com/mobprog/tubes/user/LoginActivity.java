bd7b2c212aaf30aeadf04e5bfb9cc3e0cd53a6d2
Adam Safitra <adamsafitra24@gmail.com>
2024-01-31 21:44:30 +0800

    menambahkan activity login dan registrasi

---
+++ app/src/main/java/com/mobprog/tubes/user/LoginActivity.java
@@ -0,0 +1,25 @@
+package com.mobprog.tubes.user;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mobprog.tubes.R;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
---
+++ app/src/main/java/com/mobprog/tubes/user/RegistActivity.java
@@ -0,0 +1,24 @@
+package com.mobprog.tubes.user;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mobprog.tubes.R;
+
+public class RegistActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
